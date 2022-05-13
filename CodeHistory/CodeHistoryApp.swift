60471e5d00581d7863a04b1660e616e86aac7c78
Cathy Chen <cathycat0131@gmail.com>
2022-05-12 18:09:39 -0700

    Initial Commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Cathy Chen on 2022-05-12.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeHistory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeHistory
+//
+//  Created by Cathy Chen on 2022-05-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
